eb62e3358e5e720039c960c67133300f7531c4a7
wangyw <532858859@qq.com>
2019-05-20 16:49:06 +0800

    根据坐标获取市名

--- hexie/src/main/java/com/yumu/hexie/web/user/AddressController.java
+++ hexie/src/main/java/com/yumu/hexie/web/user/AddressController.java
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yumu.hexie.common.Constants;
 import com.yumu.hexie.common.util.StringUtil;
+import com.yumu.hexie.integration.baidu.BaiduMapUtil;
 import com.yumu.hexie.model.distribution.region.AmapAddress;
 import com.yumu.hexie.model.distribution.region.Region;
 import com.yumu.hexie.model.user.Address;
@@ -133,4 +135,17 @@ public class AddressController extends BaseController{
         return BaseResult.successResult(regions);
     }
 	
+    /**
+     * 根据坐标获取市名
+     * @param coordinate
+     * @return
+     * @throws Exception
+     */
+	@RequestMapping(value = "/addres/getName", method = RequestMethod.POST)
+	@ResponseBody
+    public BaseResult<String> getAddres(@RequestParam String coordinate) throws Exception {
+		coordinate = BaiduMapUtil.findByCoordinateGetBaidu(coordinate);
+		String name = BaiduMapUtil.findByBaiduGetCity(coordinate);
+        return BaseResult.successResult(name);
+    }
 }
