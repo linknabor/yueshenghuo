f89c85546eba376dc0f2795618d0674beb469e28
davidhardson <davidhardson@hotmail.com>
2020-03-19 14:24:29 +0800

    修正字段名大小写

--- hexie/src/main/java/com/yumu/hexie/web/user/resp/UserInfo.java
+++ hexie/src/main/java/com/yumu/hexie/web/user/resp/UserInfo.java
@@ -58,7 +58,7 @@ public class UserInfo implements Serializable {
     private long id;
     private String sect_id;
     private String officeTel;
-    private String openId;
+    private String openid;
 
     public String getOfficeTel() {
 		return officeTel;
@@ -255,12 +255,12 @@ public class UserInfo implements Serializable {
 	public void setSect_id(String sect_id) {
 		this.sect_id = sect_id;
 	}
-	public String getOpenId() {
-		return openId;
+	public String getOpenid() {
+		return openid;
 	}
-	public void setOpenId(String openId) {
-		this.openId = openId;
+	public void setOpenid(String openid) {
+		this.openid = openid;
 	}
-    
+	
     
 }
